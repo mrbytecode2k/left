e2976ad2cb6aeeec8ebd3a3359d38cea91b33b33
Administrador <byte.fox@gmail.com>
2023-04-25 22:10:33 -0400

    Se modifica el archivo filter_page.dart

--- lib/src/features/data/domain/presentation/filter_page/View/filter_page.dart
+++ lib/src/features/data/domain/presentation/filter_page/View/filter_page.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+
+class FilterPage extends StatefulWidget {
+const FilterPage({Key? key }) : super(key: key);
+
+
+  @override
+  State<FilterPage> createState() => _FilterPageState();
+}
+
+class _FilterPageState extends State<FilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Hola"),
+    );
+  }
+}
