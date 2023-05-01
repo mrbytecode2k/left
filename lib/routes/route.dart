a92bd8a8cc4ed6359b2071620e7e36e90af51592
Administrador <byte.fox@gmail.com>
2023-05-01 14:14:31 -0400

    Refactorizacion

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:left/src/features/data/domain/presentation/search_page/view/searchPage.dart';
-import 'package:left/src/features/data/domain/presentation/welcome_page/view/welcome_page.dart';
-import 'package:left/src/features/data/domain/presentation/login_page/view/login_page.dart';
-import 'package:left/src/features/data/domain/presentation/tabs/tabs_page.dart';
-import 'package:left/src/features/data/domain/presentation/forgot_page/view/forgot_password_page.dart';
-import 'package:left/src/features/data/domain/presentation/signup/view/signup_page.dart';
-import 'package:left/src/features/data/domain/presentation/filter_page/View/filter_page.dart';
+import 'package:left/src/features/presentation/search_page/view/searchPage.dart';
+import 'package:left/src/features/presentation/welcome_page/view/welcome_page.dart';
+import 'package:left/src/features/presentation/login_page/view/login_page.dart';
+import 'package:left/src/features/presentation/tabs/tabs_page.dart';
+import 'package:left/src/features/presentation/forgot_page/view/forgot_password_page.dart';
+import 'package:left/src/features/presentation/signup/view/signup_page.dart';
+import 'package:left/src/features/presentation/filter_page/View/filter_page.dart';
 
 
 
