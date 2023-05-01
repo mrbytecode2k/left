221f6fd7a959fecea3be465446ee377ad2634bb0
Administrador <byte.fox@gmail.com>
2023-05-01 14:22:11 -0400

    Refactorizacion

--- lib/src/features/presentation/commons_widgets/Headers/header_double.dart
+++ lib/src/features/presentation/commons_widgets/Headers/header_double.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:left/src/features/presentation/widgets/colors.dart';
-import 'package:left/src/features/presentation/widgets/header_text.dart';
+import 'package:left/src/features/presentation/commons_widgets/colors.dart';
+import 'package:left/src/features/presentation/commons_widgets/Headers/header_text.dart';
 
 Widget HeaderDouble({String textHeader="", String textAction =""}) {
   return Container(
